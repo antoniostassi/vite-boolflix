5473da3e5fb001d9b0d6bf7964f8b9f3b65eed52
antoniostassi <stassiant@outlook.it>
2024-09-16 12:24:44 +0200

    initialising repo

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script>
+    //import TestTemplate from './components/TestTemplate.vue';
+    export default {
+        data(){
+            return{
+                
+            }
+        },
+        components:{
+            //TestTemplate
+        }
+    }
+</script>
+
+<template>
+    <h1>Test</h1>
+</template>
+
+<style lang="scss">
+    @import "bootstrap/scss/bootstrap";
+    
+</style>
